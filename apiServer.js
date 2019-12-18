102790dea2865250427a54694a100c8f38c010a0
Rex-Xinchao <13601935905@163.com>
2019-12-18 16:04:25 +0800

    add timeout time

--- apiServer.js
+++ apiServer.js
@@ -1,8 +1,25 @@
 const express = require('express');
+const timeout = require('connect-timeout');
 const proxy = require('http-proxy-middleware');
 const path = require('path');
 const { port, proxy: proxyConfig } = require('./serverConfig');
 const app = express();
+// 超时时间
+const TIME_OUT = '5s';
+
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+// 设置超时 返回超时响应
+app.use(timeout(TIME_OUT));
+app.use((req, res, next) => {
+  if (!req.timedout) next();
+});
 
 //将静态文件目录设置为：项目根目录+/public
 app.use(express.static(__dirname + '/public'));
